d3a28bd9cbb0c59850f521eb48473a133a3c05a6
Alex <alexandruionutz118@gmail.com>
2022-04-23 12:28:41 +0100

    Prints to see where is the seg fault

--- maxwell - MPI/setup.c
+++ maxwell - MPI/setup.c
@@ -47,14 +47,18 @@ void allocate_arrays() {
 	// Add ghost columns
 	Ex_size_x = X; Ex_size_y = Y+1;
 	Ex = alloc_2d_array(X+1,Y+1);
+	printf("Alloc first thingy");
 	Ey_size_x = X+1; Ey_size_y = Y;
 	Ey = alloc_2d_array(X+1,Y);
+	printf("Alloc second thingy");
 	
 	Bz_size_x = X; Bz_size_y = Y;
 	Bz = alloc_2d_array(X+1, Y);
+	printft("Alloc third thingy");
 	
 	E_size_x = X+1; E_size_y = Y+1; E_size_z = 3;
 	E = alloc_3d_array(E_size_x,E_size_y,E_size_z);
+	printf("Alloc E thingy");
 
 	B_size_x = X+1; B_size_y = Y+1; B_size_z = 3;
 	B = alloc_3d_array(B_size_x,B_size_y,B_size_z);
